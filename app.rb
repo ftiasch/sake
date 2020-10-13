2f65ebf0a49cdf0e2f07b8c7536df4fdb8a83b3e
Xiaoxu Guo <ftiasch0@gmail.com>
2020-10-13 07:35:41 -0700

    refactored

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative 'lib'
+
 require 'json'
 require 'logger'
-require 'http'
-require 'nokogiri'
 require 'yaml'
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2'
@@ -13,94 +13,38 @@ def json_parse(data)
   JSON.parse(data) if data
 end
 
-def fetch_tweets(user)
-  html = HTTP[user_agent: USER_AGENT].follow.get("https://twitter.com/#{user}").to_s
-  Nokogiri::HTML(html).css('.tweet-container').map(&:text).map(&:strip)
-rescue StandardError
-  []
-end
-
-def filter_tweets(tweets, tags)
-  tweets.filter do |tweet|
-    tags.map do |tag|
-      tweet.include? tag
-    end.any?
-  end
-end
-
-def concat_tweets(tweets, user, tags)
-  index = tweets.index { |t| !reply?(t) }
-  tweets[0..index].reverse.each_with_index.map { |t, i| clean t, i, user, tags }.join("\n")
-end
-
-def reply?(tweet)
-  tweet.include? 'Replying to'
-end
-
-def clean(tweet, index, user, tags)
-  return tweet if index.zero?
-
-  tweet = tweet.clone
-  tags.each { |t| tweet.gsub!(t, '') }
-  tweet.gsub(Regexp.new("^Replying to[\\n\s]*@#{user}"), '').strip
-end
-
-def post_telegram(api_token, chat_id, text)
-  JSON.parse(HTTP.post("https://api.telegram.org/bot#{api_token}/sendMessage", json: {
-                         chat_id: chat_id,
-                         text: text
-                       }).to_s)['ok']
-rescue StandardError
-  false
-end
-
-def healthcheck(url)
-  return true unless url
-
-  HTTP.get url
-rescue StandardError
-  false
-end
-
 if __FILE__ == $PROGRAM_NAME
   logger = if ENV['DOCKER_LOGS']
              Logger.new '/proc/1/fd/1'
            else
-             Logger.new STDOUT
+             Logger.new $stdout
            end
   logger.level = Logger::INFO
 
   config = YAML.safe_load File.open('config.yaml').read
   twitter_user = config['twitter_user']
   twitter_tags = config['twitter_tags']
+  post_first = ENV['SAKE_POST_FIRST']
   telegram_api_token = ENV['SAKE_TELEGRAM_API_TOKEN']
   telegram_channel = json_parse(ENV['SAKE_TELEGRAM_CHANNEL_JSON']) || config['telegram_channel']
+  healthchecks_url = ENV['SAKE_HEALTHCHECKS_URL']
   unless telegram_api_token
     logger.fatal 'No `SAKE_TELEGRAM_API_TOKEN` found in ENV.'
     abort
   end
-  healthchecks_url = ENV['SAKE_HEALTHCHECKS_URL']
 
-  last_tweet = nil
+  twitter = Twitter.new twitter_user, twitter_tags
+  telegram = Telegram.new post_first, telegram_api_token, telegram_channel
+  healthchecks = Healthchecks.new healthchecks_url
   loop do
-    tweets = fetch_tweets(twitter_user)
-    filtered_tweets = filter_tweets(tweets, twitter_tags)
-    if filtered_tweets.empty?
+    tweets = twitter.fetch
+    if tweets.empty?
       logger.info 'No tweets.'
     else
-      new_tweet = concat_tweets(filtered_tweets, twitter_user, twitter_tags)
-      if last_tweet != new_tweet
-        if post_telegram(telegram_api_token, telegram_channel, new_tweet)
-          logger.info 'Success to post.'
-          last_tweet = new_tweet
-        else
-          logger.error 'Failed to post. Try next time.'
-        end
-      else
-        logger.info "Already posted. #{new_tweet.split.join('')}"
-      end
+      new_tweet = twitter.last_tweet tweets
+      telegram.post new_tweet
     end
-    sleep 60 until healthcheck(healthchecks_url)
+    healthchecks.check
     sleep 900 # 15 minutes
   end
 end
---
+++ test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require_relative 'lib'
+
+require 'test/unit'
+
+class TestTwitter < Test::Unit::TestCase
+  def setup
+    @twitter = Twitter.new 'Misa2_omoshiroi', ['#misa国日报',
+                                               '#misa国午夜电台']
+    @raw_tweets = ['叉叉是个痴情宝宝！',
+                   "#misa国日报\n13 Oct 2020\n1.#misa国花边小报 暂试行，详情关注叉叉！\n2.misa国今日关注：要开发布会啦。\n3.misa状态：努力战胜自己hhh\n4.misa国今日建议：不要对自己深爱的人留下什么殉情的念想，拜托了。\n5.我很害怕，始终害怕连累你，初见就是如此，一直如此。但...此时我也变得不能接受变成回忆了。",
+                   "Replying to\n          @Misa2_omoshiroi\n\n        \n        \n\n      \n\n    \n        #misa国午夜电台\n一帆风顺的我，没想到妈妈的反应会如此的夸张，夸张到她拿了一把刀好像真的要砍我一样。\n到底是不是真的？如果我不打飞那把刀，我会留下什么一生难忘的东西吗？\n那无所谓了。\n突然发现世界最亲的人失去了可以沟通的筹码，才是一切的开始。\n会发现自己越来越孤独。\n已经，十几年了呢。",
+                   "#misa国午夜电台\n抑郁。\n我的一切精神问题，源自那个有些冷的冬夜。\n那时，我是被宠坏的孩子，无论是家庭还是天赋，都是随心所欲的样子。\n不断的凭着天赋在探索着与我年龄严重不符的世界。\n至于为什么是性不是学术，为什么非要喜欢女人还会记录在手机上。\n我觉得，这只是一种冥冥之中的自然选择罢了。"]
+    @tweets = @raw_tweets.map { |t| @twitter.make_tweet t }
+  end
+
+  def test_tweet
+    assert_equal @tweets, @raw_tweets
+  end
+
+  def test_reply
+    assert_false @tweets[1].reply?
+    assert_true @tweets[2].reply?
+  end
+
+  def test_tagged
+    assert_false @tweets[0].tagged?
+    assert_true @tweets[1].tagged?
+  end
+
+  def test_strip_reply
+    tweet = @tweets[1].strip_reply
+    assert_false tweet.reply?
+    assert_false tweet.tagged?
+  end
+
+  def test_last_tweet
+    puts @twitter.last_tweet [@tweets[0], @tweets[2], @tweets[3], @tweets[1]]
+  end
+end
+
+class TestTelegram < Test::Unit::TestCase
+  def setup
+    @telegram = Telegram.new ENV['SAKE_TELEGRAM_API_TOKEN'], '@sake_test_channel'
+  end
+
+  # def test_post
+  #   @telegram.post 'test message'
+  # end
+end
