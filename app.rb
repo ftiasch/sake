e215c142710417891363f1f208ddad7b237acbc7
Xiaoxu Guo <ftiasch0@gmail.com>
2020-12-16 22:11:40 -0800

    removed duplicated user-agent string

--- app.rb
+++ app.rb
@@ -7,8 +7,6 @@ require 'json'
 require 'logger'
 require 'yaml'
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2'
-
 def json_parse(data)
   JSON.parse(data) if data
 end
